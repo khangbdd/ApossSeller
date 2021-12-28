1888d885198e1db37947b09f3560d4e2c21969cf
tanpmclk14uit <tan.lk16.cla@gmail.com>
2021-12-28 14:32:04 +0700

    done order manage

--- app/src/main/java/com/example/apossseller/repository/webservice/OrderService.kt
+++ app/src/main/java/com/example/apossseller/repository/webservice/OrderService.kt
@@ -8,42 +8,11 @@ import retrofit2.http.*
 
 interface OrderService {
 
-    @POST("order")
-    suspend fun addNewOrder(
-        @Body orderDTO: OrderDTO,
-        @Header("Authorization") token: String,
-    ): Response<String>
-
-    @PUT("order/hold")
-    suspend fun holdProduct(
-        @Body listOrderItemDTO: List<OrderItemDTO>,
-        @Header("Authorization") token: String,
-    ): Response<String>
-
-    @PUT("order/reduceHold")
-    suspend fun reduceHold(
-        @Body listOrderItemDTO: List<OrderItemDTO>,
-        @Header("Authorization") token: String,
-    ): Response<String>
-
-    @POST("order/all-order-by")
-    suspend fun getAllOrderByStatus(
-        @Body orderStatus: OrderStatus,
-        @Header("Authorization") token: String,
-    ): Response<List<OrderDTO>>
-
     @GET("order/order-by-id/{id}")
     suspend fun getOrderById(
         @Header("Authorization") accessToken: String,
         @Path ("id") id: Long
     ): Response<OrderDTO>
-
-    @POST("order/cancel-order-customer/{id}")
-    suspend fun cancelOrder(
-        @Path(value = "id") id: Long,
-        @Body cancelReason: String,
-        @Header("Authorization") accessToken: String,
-    ): Response<String>
 
     @PUT("order/change-order-status/{id}")
     suspend fun changeOrderStatus(
